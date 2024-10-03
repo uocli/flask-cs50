badb5315fa67530ab1b8dcc21b44b5cfbe55a1d7
Sean Li <cli049@uottawa.ca>
2024-10-02 21:31:50 -0400

    POST and GET within a single page

--- app.py
+++ app.py
@@ -3,12 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form.get("name", "world")
+        return render_template('greet.html', name=name)
     return render_template('index.html')
-
-
-@app.route("/greet", methods=["POST"])
-def greet():
-    name = request.form.get('name', 'world')
-    return render_template("greet.html", name=name)
