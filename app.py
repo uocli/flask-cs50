4b8b1252536f4cd69291580a5ac97aaab44a1799
Sean Li <cli049@uottawa.ca>
2024-10-02 21:19:52 -0400

    Templates

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route("/greet")
+def greet():
+    name = request.args.get('name', 'world')
+    return render_template("greet.html", name=name)
